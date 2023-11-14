20b20ea40d261d82e9a3dda5296e26679782959c
Jane <Zhanna.v.akimenko@gmail.com>
2023-11-14 15:43:15 +0700

    Finished table resize functional

--- src/components/table/table.template.js
+++ src/components/table/table.template.js
@@ -3,48 +3,42 @@ const CODES = {
     Z: 90
 }
 
+function toCell(_, col) {
+    return `
+    <div class="cell" contenteditable data-col="${col}"></div>
+  `
+}
+
+function toColumn(col, index) {
+    return `
+    <div class="column" data-type="resizable" data-col="${index}">
+      ${col}
+      <div class="col-resize" data-resize="col"></div>
+    </div>
+  `
+}
+
 function createRow(index, content) {
-    return `<div class="row">
-        <div class="row-info">${index ? index : ''}</div>
-        <div class="row-data">${content}</div>
-        </div>
-            `
-}
-
-// Мое решение. Не забыть удалить!
-// function createHeaderRow() {
-//     let letter = ''
-//     const headerRow = []
-//
-//     for (let i = 65; i <= 90; i++) {
-//         letter = String.fromCharCode(i)
-//         headerRow.push(`<div class="row-info">${letter}</div>`)
-//     }
-//
-//     return `<div class="row">
-//         <div class="row-info">1</div>
-//         ${headerRow.join('')}
-//         </div>
-//             `
-// }
-
-function toCell() {
+    const resize = index
+        ? '<div class="row-resize" data-resize="row"></div>'
+        : ''
     return `
-      <div class="cell" contenteditable></div>
-    `
-}
-
-function toColumn(col) {
-    return `
-    <div class="column">${col}</div>
-    `
+    <div class="row" data-type="resizable">
+      <div class="row-info">
+        ${index ? index : ''}
+        ${resize}
+      </div>
+      <div class="row-data">${content}</div>
+    </div>
+  `
 }
 
 function toChar(_, index) {
     return String.fromCharCode(CODES.A + index)
 }
-export function createTable(rowsCount = 10) {
-    const colsCount = CODES.Z - CODES.A + 1
+
+export function createTable(rowsCount = 15) {
+    const colsCount = CODES.Z - CODES.A + 1 // Compute cols count
     const rows = []
 
     const cols = new Array(colsCount)
@@ -55,13 +49,13 @@ export function createTable(rowsCount = 10) {
 
     rows.push(createRow(null, cols))
 
-    for (let i = 1; i <= rowsCount; i++) {
+    for (let i = 0; i < rowsCount; i++) {
         const cells = new Array(colsCount)
             .fill('')
             .map(toCell)
             .join('')
 
-        rows.push(createRow(i, cells))
+        rows.push(createRow(i + 1, cells))
     }
 
     return rows.join('')
